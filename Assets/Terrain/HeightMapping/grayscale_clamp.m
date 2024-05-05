6aa92ccd7e9b432aa9c70972257d3b123204b915
JohnnyDingYQ <johnnydyq@outlook.com>
2024-05-05 17:44:32 -0400

    terrain improvement

---
+++ Assets/Terrain/HeightMapping/grayscale_clamp.m
@@ -0,0 +1,16 @@
+img = imread("in.png", "png");
+threshold = 2000;
+clamp_high = 2000;
+clamp_low = 0;
+img = imresize(img, 4, "bicubic");
+for i = 1:numel(img)
+    h = img(i);
+    if h > threshold
+        img(i) = clamp_high;
+    else
+        img(i) = clamp_low;
+    end
+end
+% img = imbilatfilt(img);
+imshow(img);
+imwrite(img, "out.png", "png");
