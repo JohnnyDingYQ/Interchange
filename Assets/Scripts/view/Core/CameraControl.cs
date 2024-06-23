2d0a8debe230a1def49f56fa5a2fe96807a4e382
JohnnyDingYQ <johnnydyq@outlook.com>
2024-06-23 08:51:31 -0400

    Zoom towards mouse pos

--- Assets/Scripts/view/Core/CameraControl.cs
+++ Assets/Scripts/view/Core/CameraControl.cs
@@ -6,7 +6,6 @@ public class CameraControl : MonoBehaviour
 {
     [SerializeField]
     CameraSettings cameraSettings;
-    private const float CameraSpeedMultiplier = 10;
     public static Vector3 CameraOffset;
 
     void Update()
@@ -22,10 +21,32 @@ public class CameraControl : MonoBehaviour
         cameraOffset.z *= cameraSettings.PanMultiplier * cameraSettings.PanSpeed.Evaluate(heightRatio);
         cameraOffset.y *= cameraSettings.ZoomMultiplier * cameraSettings.ZoomSpeed.Evaluate(heightRatio);
 
-        float3 newCameraPos = Camera.main.transform.position + CameraSpeedMultiplier * Time.deltaTime * cameraOffset;
-        newCameraPos.y = Math.Clamp(newCameraPos.y, cameraSettings.MinHeight, cameraSettings.MaxHeight);
-        
-        Camera.main.transform.position = newCameraPos;
-        Camera.main.orthographicSize = newCameraPos.y;
+        ApplyZoom();
+        ApplyPan();
+        ClampToBounds();
+
+        Camera.main.orthographicSize = Camera.main.transform.position.y;
+
+        void ApplyZoom()
+        {
+            Vector3 v = Camera.main.transform.position - (Vector3)InputSystem.MouseWorldPos;
+            float t = (cameraHeight + cameraOffset.y - Camera.main.transform.position.y) / v.y;
+            Camera.main.transform.position +=  t * Time.deltaTime * v;
+        }
+
+        void ApplyPan()
+        {
+            float3 newCameraPos = Camera.main.transform.position;
+            newCameraPos.x += Time.deltaTime * cameraOffset.x;
+            newCameraPos.z += Time.deltaTime * cameraOffset.z;
+            Camera.main.transform.position = newCameraPos;
+        }
+
+        void ClampToBounds()
+        {
+            float3 cameraPos = Camera.main.transform.position;
+            cameraPos.y = Math.Clamp(cameraPos.y, cameraSettings.MinHeight, cameraSettings.MaxHeight);
+            Camera.main.transform.position = cameraPos;
+        }
     }
 }
